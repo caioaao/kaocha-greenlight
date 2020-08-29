63b26192e471a3ceeb86f5569b81be222f7c599d
Rob Hanlon <robhanlon@gmail.com>
2020-08-28 19:22:59 -0700

    Use api/run in type_test as well

--- test/caioaao/kaocha_greenlight/type_test.clj
+++ test/caioaao/kaocha_greenlight/type_test.clj
@@ -1,8 +1,9 @@
 (ns caioaao.kaocha-greenlight.type-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
             [com.stuartsierra.component :as component]
             [kaocha.testable :as testable]
-            [kaocha.result]
+            [kaocha.api :as api]
+            [kaocha.result :as result]
             [matcher-combinators.test]
             [matcher-combinators.parser :refer [mimic-matcher]]
             [caioaao.kaocha-greenlight.test-suite.blue-test]
@@ -11,32 +12,37 @@
 
 (mimic-matcher matchers/equals clojure.lang.Var)
 
-(defn new-system-1 [& _]
+(defn new-system-1
+  [& _]
   (component/system-map :greenlight.test-test/component 6))
 
-(defn new-system-2 [& _]
+(defn new-system-2
+  [& _]
   (component/system-map :greenlight.test-test/component 5))
 
-(def test-suite-blue {::testable/type                       :caioaao.kaocha-greenlight/test
-                      ::testable/id                         :integration-blue
-                      :caioaao.kaocha-greenlight/new-system 'caioaao.kaocha-greenlight.type-test/new-system-1
-                      :kaocha/ns-patterns                   ["test-suite.blue-test$"]
-                      :kaocha/source-paths                  ["src"]
-                      :kaocha/test-paths                    ["test"]})
+(def test-suite-blue
+  {::testable/type                       :caioaao.kaocha-greenlight/test
+   ::testable/id                         :integration-blue
+   :caioaao.kaocha-greenlight/new-system 'caioaao.kaocha-greenlight.type-test/new-system-1
+   :kaocha/ns-patterns                   ["test-suite.blue-test$"]
+   :kaocha/source-paths                  ["src"]
+   :kaocha/test-paths                    ["test"]})
 
-(def test-suite-red {::testable/type                       :caioaao.kaocha-greenlight/test
-                     ::testable/id                         :integration-red
-                     :caioaao.kaocha-greenlight/new-system 'caioaao.kaocha-greenlight.type-test/new-system-2
-                     :kaocha/ns-patterns                   ["test-suite.red-test$"]
-                     :kaocha/source-paths                  ["src"]
-                     :kaocha/test-paths                    ["test"]})
+(def test-suite-red
+  {::testable/type                       :caioaao.kaocha-greenlight/test
+   ::testable/id                         :integration-red
+   :caioaao.kaocha-greenlight/new-system 'caioaao.kaocha-greenlight.type-test/new-system-2
+   :kaocha/ns-patterns                   ["test-suite.red-test$"]
+   :kaocha/source-paths                  ["src"]
+   :kaocha/test-paths                    ["test"]})
 
-(def full-test-suite {::testable/type                       :caioaao.kaocha-greenlight/test
-                      ::testable/id                         :integration-full
-                      :caioaao.kaocha-greenlight/new-system 'caioaao.kaocha-greenlight.type-test/new-system-1
-                      :kaocha/ns-patterns                   ["test-suite.*-test$"]
-                      :kaocha/source-paths                  ["src"]
-                      :kaocha/test-paths                    ["test"]})
+(def full-test-suite
+  {::testable/type                       :caioaao.kaocha-greenlight/test
+   ::testable/id                         :integration-full
+   :caioaao.kaocha-greenlight/new-system 'caioaao.kaocha-greenlight.type-test/new-system-1
+   :kaocha/ns-patterns                   ["test-suite.*-test$"]
+   :kaocha/source-paths                  ["src"]
+   :kaocha/test-paths                    ["test"]})
 
 (deftest loading-tests
   (testing "contains test suite info"
@@ -67,31 +73,31 @@
     (is (= "integration-red (greenlight)"
            (:kaocha.testable/desc (testable/load test-suite-red))))))
 
-(let [blue-test-plan    (testable/load test-suite-blue)
-      red-test-plan     (testable/load test-suite-red)
-      full-test-plan    (testable/load full-test-suite)
-      blue-results      (testable/run blue-test-plan blue-test-plan)
-      red-results       (testable/run red-test-plan red-test-plan)
-      full-test-results (testable/run full-test-plan full-test-plan)]
-  (deftest running-tests
-    (testing "counts are correctly tracked"
-      (is (match? #:kaocha.result{:count   1
-                                  :error   0
-                                  :fail    0
-                                  :pass    13
-                                  :pending 0}
-                  (kaocha.result/testable-totals blue-results)))
+(deftest running-tests
+  (testing "counts are correctly tracked"
+    (is (match? #:kaocha.result{:count   1
+                                :error   0
+                                :fail    0
+                                :pass    13
+                                :pending 0}
+                (-> {:kaocha/tests [test-suite-blue]}
+                    api/run
+                    result/testable-totals)))
 
-      (is (match? #:kaocha.result{:count   1
-                                  :error   0
-                                  :fail    1
-                                  :pass    9
-                                  :pending 0}
-                  (kaocha.result/testable-totals red-results)))
+    (is (match? #:kaocha.result{:count   1
+                                :error   0
+                                :fail    1
+                                :pass    9
+                                :pending 0}
+                (-> {:kaocha/tests [test-suite-red]}
+                    api/run
+                    result/testable-totals)))
 
-      (is (match? #:kaocha.result{:count   2
-                                  :error   0
-                                  :fail    0
-                                  :pass    26
-                                  :pending 0}
-                  (kaocha.result/testable-totals full-test-results))))))
+    (is (match? #:kaocha.result{:count   2
+                                :error   0
+                                :fail    0
+                                :pass    26
+                                :pending 0}
+                (-> {:kaocha/tests [full-test-suite]}
+                    api/run
+                    result/testable-totals)))))
