f13440d757781edc65f2fb905e7052897c8f76cd
Rob Hanlon <robhanlon@gmail.com>
2020-08-29 17:10:23 -0700

    Correctly report uncaught error outcomes

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [amperity/greenlight "0.4.0"]
+                 [amperity/greenlight "0.5.0"]
                  [lambdaisland/kaocha "1.0.672"]]
   :aliases {"test" ["run" "-m" "kaocha.runner"]}
   :profiles {:dev {:dependencies [[nubank/matcher-combinators "0.4.2"]
