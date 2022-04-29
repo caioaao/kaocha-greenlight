eeb69491069df7c0b57d577f7b6475f450417a6a
Rob Hanlon <robhanlon@gmail.com>
2022-04-29 10:19:50 -0700

    Bumping dependencies in preparation for next version

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "http://github.com/caioaao/kaocha-greenlight"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [amperity/greenlight "0.6.1"]
-                 [lambdaisland/kaocha "1.0.672"]]
+                 [lambdaisland/kaocha "1.66.1034"]]
   :aliases {"test" ["run" "-m" "kaocha.runner"]}
-  :profiles {:dev {:dependencies [[nubank/matcher-combinators "0.4.2"]
-                                  [orchestra "2020.07.12-1"]
+  :profiles {:dev {:dependencies [[nubank/matcher-combinators "3.5.0"]
+                                  [orchestra "2021.01.01-1"]
                                   [org.clojure/test.check "1.1.0"]
-                                  [expound "0.8.5"]]
+                                  [expound "0.9.0"]]
                    :source-paths ["dev"]
-                   :plugins      [[lein-cljfmt "0.6.1"]]}}
+                   :plugins      [[lein-cljfmt "0.8.0"]]}}
   :repl-options {:init-ns kaocha-greenlight.core}
   :release-tasks [["deploy" "clojars"]]
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
